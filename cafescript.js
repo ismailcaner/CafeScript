00ee46d137482d994ba63fc5ae5c57b4982f18fa
ismailcaner <ismailcaner97@gmail.com>
2024-03-23 05:26:43 +0300

    first commit

---
+++ cafescript.js
@@ -0,0 +1,17 @@
+function anasayfa() {
+    location.reload();
+}
+
+function kahvelerdiv() {
+    document.getElementById('coffees').scrollIntoView({ behavior: 'smooth' });
+}
+
+function hakkındadiv() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
+function iletisim() {
+    document.getElementById('iletisim').scrollIntoView({ behavior: 'smooth' });
+}
+
+
