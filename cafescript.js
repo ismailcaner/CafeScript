941fda517103c43ebb669bf1768266efba10a225
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2024-04-05 01:16:33 +0300

    Update cafescript.js

--- cafescript.js
+++ cafescript.js
@@ -1,17 +1,31 @@
+
+function dropdown(){
+    var drop = document.getElementById("dropdownContent")
+    drop.classList.remove('show')
+}
+
 function anasayfa() {
     location.reload();
+    dropdown()
 }
 
 function kahvelerdiv() {
     document.getElementById('coffees').scrollIntoView({ behavior: 'smooth' });
+    dropdown()
 }
 
 function hakkındadiv() {
     document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+    dropdown()
 }
 
 function iletisim() {
     document.getElementById('iletisim').scrollIntoView({ behavior: 'smooth' });
+    dropdown()
 }
 
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownContent");
+    dropdownContent.classList.toggle("show");
+}
 
